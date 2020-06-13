0b5fc763e88b36421a77bc955607f9aefde48f7e
AKozlov <a.kozlov@8bitov.com>
2020-06-13 19:13:00 +0300

    Updates

---
+++ precache-manifest.93cfd9c0b6e4f750f495116b0dd25dda.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed76521e00d5da5532c98b11212147e",
+    "url": "/arcch-land/index.html"
+  },
+  {
+    "revision": "46f2448fef3e03a825fb",
+    "url": "/arcch-land/static/css/main.9b55e439.chunk.css"
+  },
+  {
+    "revision": "cd6222ee580516b117c5",
+    "url": "/arcch-land/static/js/2.0ae3190a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arcch-land/static/js/2.0ae3190a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f2448fef3e03a825fb",
+    "url": "/arcch-land/static/js/main.e3a8ad0b.chunk.js"
+  },
+  {
+    "revision": "5576833d5010838852c5",
+    "url": "/arcch-land/static/js/runtime-main.4659d800.js"
+  },
+  {
+    "revision": "c59cb4c82e76cfe9b543e04bf35a25fd",
+    "url": "/arcch-land/static/media/1.c59cb4c8.jpg"
+  },
+  {
+    "revision": "cb8b8ab6f78589181d801a3b986dfe29",
+    "url": "/arcch-land/static/media/2.cb8b8ab6.jpg"
+  },
+  {
+    "revision": "c4a597bc1317d4cadd9512d1e6a4c8b8",
+    "url": "/arcch-land/static/media/3.c4a597bc.jpg"
+  },
+  {
+    "revision": "735f1a49d97304bc6b8a09c69b90c378",
+    "url": "/arcch-land/static/media/4.735f1a49.jpg"
+  },
+  {
+    "revision": "f3191841b2b71f63cba8351fc5e4002f",
+    "url": "/arcch-land/static/media/5.f3191841.jpg"
+  },
+  {
+    "revision": "8423d5a172c39f13b21768e8cf48f4f4",
+    "url": "/arcch-land/static/media/logo.8423d5a1.svg"
+  }
+]);
